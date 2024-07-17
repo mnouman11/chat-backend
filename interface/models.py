87c4693aa2786cefa45a25697c20b0de2cf188be
mnouman11 <noumang125@gmail.com>
2024-07-18 00:12:46 +0500

    Model added for chat

--- interface/models.py
+++ interface/models.py
@@ -10,3 +10,19 @@ class MyUser(AbstractUser):
     # Add any other fields or custom methods if needed
     def __str__(self):
         return self.username
+    
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    members = models.BigIntegerField(null=True, blank=True)
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    chat = models.ForeignKey(ChatRoom, related_name='messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(MyUser, related_name='sender', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content[:50]}'
